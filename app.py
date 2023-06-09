2cb1bea4de6ecb67ad7207d4095285564f5f4270
UBC Student <medlin@students.cs.ubc.ca>
2023-06-08 17:48:52 -0700

    Update app.py
    added skeleton to change wallpaper based on sunset/sunrise time

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
 import requests
+import time
 
 api_key = '09118d9f8d6629d10ca73bfece254da6'
 
 weather_data = requests.get(
-    f"https://api.openweathermap.org/data/2.5/onecall?lat=49.21&lon=-123.03&appid={api_key}")
+    f"https://api.openweathermap.org/data/2.5/weather?q=Vancouver&APPID={api_key}")
 
-sunrise = weather_data.xml()['sys']['sunrise']
-sunset = weather_data.xml()['sys']['sunset']
-timezone = weather_data.xml()['sys']['timezone']
+#given in unix, UTC
+sunrise = weather_data.json()['sys']['sunrise']
+sunset = weather_data.json()['sys']['sunset']
+timezone = weather_data.json()['timezone']
 
-print(weather_data)
+print(sunrise)
+print(sunset)
+print(timezone)
+
+#also in unix, UTC
+curr_time = time.time()
+print(curr_time)
+
+if curr_time < sunrise:
+    #set wallpaper to presunrise
+    print()
+elif sunrise - 900 < curr_time < sunrise + 900:
+    #set wallpaper to sunrise
+    print()
+elif sunrise + 900 <= curr_time <= sunset - 1800:
+    #set wallpaper to daytime
+    print()
+elif sunset - 1800 < curr_time < sunset + 1800:
+    #set wallpaper to daytime
+    print()
+
